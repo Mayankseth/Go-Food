2efa5a50e807b60262c49597012e7c96acab9800
Mayankseth <sethmayank33@gmail.com>
2023-12-30 11:11:17 +0530

    carousel

--- gofood/src/screens/Home.js
+++ gofood/src/screens/Home.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import Card from "../components/Card";
+import Carousel from '../components/Carousel';
 
 export default function Home() {
   return (
@@ -9,21 +11,14 @@ export default function Home() {
         <Navbar />
       </div>
       <div>
-        <div className="card mt-3" style={{"width": "18rem", "maxHeight":"360px"}}>
-          <img src="..." className="card-img-top" alt="..." />
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">
-              Some quick example text to build on the card title. 
-            </p>
-            <div className='container w-100'>
-                <select className='m-2 h-100 w-100 bg-success'>
-                    
-                </select>
-            </div>
-          </div>
-        </div>
+        <Carousel/>
       </div>
+        <div className="m-4">
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+        </div>
       <div>
         <Footer />
       </div>
