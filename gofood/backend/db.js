9e2e0181e42ba64f77909200bc5e1b09517def97
Mayankseth <sethmayank33@gmail.com>
2024-01-04 22:21:44 +0530

    err

--- gofood/backend/db.js
+++ gofood/backend/db.js
@@ -1,11 +1,15 @@
-const mongoose  = require('mongoose');
-const mongoURI ='mongodb+srv://MERNgofood:gofoodpassword@cluster0.d4bgsz0.mongodb.net/?retryWrites=true&w=majority'
+const mongoose = require('mongoose');
 
-const mongoDB =() =>{
-    mongoose.connect(mongoURI, ()=>{
-        console.log('Connected');
-    });
-}
+const mongoURI = 'mongodb+srv://MERNgofood:gofoodpassword@cluster0.d4bgsz0.mongodb.net/?retryWrites=true&w=majority';
 
+const mongoDB = async () => {
+    try {
+        await mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error.message);
+    }
+};
 
-module.exports= mongoDB;
+module.exports = mongoDB;
+
