98a337bca036a77b0453a4ff9c57055956701f9e
Mayankseth <sethmayank33@gmail.com>
2024-01-05 17:30:50 +0530

    schema

--- gofood/backend/db.js
+++ gofood/backend/db.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
 
-const mongoURI = 'mongodb+srv://MERNgofood:gofoodpassword@cluster0.d4bgsz0.mongodb.net/?retryWrites=true&w=majority';
+const mongoURI = 'mongodb+srv://MERNgofood:gofoodpassword@cluster0.d4bgsz0.mongodb.net/merngofood?retryWrites=true&w=majority';
 
 const mongoDB = async () => {
     try {
         await mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
         console.log('Connected to MongoDB');
+        const fetched_data = await mongoose.connection.db.collection("food_items");
+        fetched_data.find({}).toArray(function(err,data){
+            if(err) console.log(err);
+            else console.log();
+        })
     } catch (error) {
         console.error('Error connecting to MongoDB:', error.message);
     }
