26e05e6e5d248a9b8711691eb79b604af697f450
Mayankseth <sethmayank33@gmail.com>
2024-01-11 16:54:54 +0530

    bjkkjb

--- gofood/src/screens/Home.js
+++ gofood/src/screens/Home.js
@@ -2,78 +2,137 @@ import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Card from "../components/Card";
-import Carousel from '../components/Carousel';
 
 export default function Home() {
-  const [foodCat,setFoodCat] = useState([]);
-  const [foodItem,setFoodItem] = useState([]);
 
-  const loadData = async ()=>{
-    let response = await fetch("http://localhost:5000/api/foodData",{
-      method:"POST",
-      headers:{
-        'Content-Type':'application/json'
+  const [search, setSearch] = useState('');
+  const [foodCat, setFoodCat] = useState([]);
+  const [foodItem, setFoodItem] = useState([]);
 
+  const loadData = async () => {
+    let response = await fetch("http://localhost:5000/api/foodData", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
       }
     });
     response = await response.json();
     setFoodItem(response[0]);
     setFoodCat(response[1]);
-    // console.log(response[0],response[1]);
   }
-  useEffect(()=>{
-    loadData()
-  },[])
 
-
-
-
-
-
+  useEffect(() => {
+    loadData();
+  }, []);
 
   return (
     <div>
+      <Navbar />
       <div>
-        <Navbar />
-      </div>
-      <div>
-        <Carousel/>
-      </div>
-        <div className="container">
-           {
-            foodCat !=[]
-            ? foodCat.map((data)=>{
-              return( <div>
-                <div key={data._id} className="fs-3 m-3">
-                  {data.CategoryName}</div>
-                  <hr/>
-                  {foodItem != []? 
-                  foodItem.filter((item)=> item.CategoryName=== data.CategoryName)
-                  .map(filterItems =>{
-                    return(
-                      <div key={filterItems._id}>
-                        <Card></Card>
-                        </div>
-                    )
-                  })
-                  
-                  
-                  
-                  : <div>No such Data found</div> }
-
-                  </div>
-
-
-              )
-            })
-            : ""
-
-           }
-            <Card/>
+      <div
+        id="carouselExampleFade"
+        className="carousel slide carousel-fade"
+        data-bs-ride="carousel"
+        style={{objectFit:"contain !important"}}
+      >
+        <div className="carousel-inner" id="carousel">
+          <div class="carousel-caption" style={{zIndex:"10"}}>
+            <div class="d-flex">
+              <input
+                class="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+                value={search}
+                onChange={(e)=> {setSearch(e.target.value)}}
+              />
+              {/* <button class="btn btn-outline-success" type="submit">
+                Search
+              </button> */}
+            </div>
+          </div>
+          <div className="carousel-item active">
+            <img
+              src="https://source.unsplash.com/random/900×700/?burger"
+              className="d-block w-100"
+              alt="..."
+              style={{filter:"brightness(50%)"}}
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="https://source.unsplash.com/random/900×700/?coffee"
+              className="d-block w-100"
+              alt="..."
+              style={{filter:"brightness(50%)"}}
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="https://source.unsplash.com/random/900×700/?pizza"
+              className="d-block w-100"
+              alt="..."
+              style={{filter:"brightness(50%)"}}
+            />
+          </div>
         </div>
-      <div>
-        <Footer />
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleFade"
+          data-bs-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleFade"
+          data-bs-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
+        </button>
       </div>
+    
+      </div>
+
+
+      <div className="container">
+        {foodCat !== [] ? (
+          foodCat.map((data) => (
+            <div className="row mb-3" key={data._id}>
+              <div className="fs-3 m-3">{data.CategoryName}</div>
+              <hr />
+              {foodItem !== [] ? (
+                foodItem
+                  .filter((item) => (item.CategoryName === data.CategoryName)  && (item.name.toLowerCase().includes(search.toLocaleLowerCase()))  )
+                  .map((filterItems) => (
+                    <div key={filterItems._id} className="col-12 col-md-6 col-lg-3">
+                      <Card
+                        foodName={filterItems.name}
+                        options={filterItems.options[0]}
+                        imgSrc={filterItems.img}
+                      />
+                    </div>
+                  ))
+              ) : (
+                <div>No such Data found</div>
+              )}
+            </div>
+          ))
+        ) : (
+          ""
+        )}
+      </div>
+      <Footer />
     </div>
   );
 }
